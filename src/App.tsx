9d92d3ffe7284c1c9d2c0ab81c2da394974dc272
ejsoh <megamisoh@fastlane.kr>
2023-09-27 14:35:14 +0900

    feat: 유틸 컨텍스트 생성 및 페이지파일 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { RecoilRoot } from "recoil";
+import Main from "./pages/Main";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-     helloWorld
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <React.Suspense fallback={<div>...loading</div>}>
+          <Main />
+        </React.Suspense>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
 
